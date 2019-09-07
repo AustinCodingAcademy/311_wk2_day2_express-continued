6f9fa947c380c62898b3d1f9d73192d2c03924c3
grantkee <grant.d.kee@gmail.com>
2019-09-07 14:58:21 -0500

    everything is connected now and I receive json files for the main directories comments, vehicles, etc. next, I need to define my :routes

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {comments} = require('./data/comments');
+const {contacts} = require('./data/contacts');
+const {products} = require('./data/products');
+const {vehicles} = require('./data/vehicles');
 const app = express();
+
+console.log(comments)
 
 const port = process.env.PORT || 4001;
 
+//pulling in additional js files for organization's sake
+app.use('/', express.static('public'));
+
+//using body-parser to serve json-style
+app.use(bodyParser.json());
+
+//get routes
+app.get('/comments', (req, res) => {
+    console.log('comments' + comments)
+    res.json(comments)
+})
+app.get('/contacts', (req, res) => {
+    res.json(contacts)
+})
+app.get('/products', (req, res) => {
+    res.json(products)
+})
+app.get('/vehicles', (req, res) => {
+    res.json(vehicles)
+})
+
+//more routes
+app.get('/:route', (req, res) => {
+	let route;
+
+	if ( req.params.route === "comments" ) { route = comments }
+	if ( req.params.route === "contacts" ) { route = contacts }
+	if ( req.params.route === "products" ) { route = products }
+	if ( req.params.route === "vehicles" ) { route = vehicles }
+
+	res.json( route );
+
+})
+
+//puting listening port down here
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
 });
