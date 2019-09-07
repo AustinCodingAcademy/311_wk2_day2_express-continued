8e9c3672a5dd3213b9968fe4bdd432c6a2518d05
jaywiles <jay.wiles@gmail.com>
2019-09-07 11:42:20 -0500

    made it through body-parser at top i think

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+app.use(express.static('public'))
 
 const port = process.env.PORT || 4001;
+
+app.use(express.static('public'))
+
+app.use(bodyParser.json());
+
+
+
+
 
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
