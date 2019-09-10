6b965310645010c9b726f413cf105c7cb258214f
nae deshazer <naedeshazer@naes-MBP.attlocal.net>
2019-09-09 19:24:39 -0500

    NewestG

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+// const express = require("express");
+// const app = express.static();
+// const port = process.env.PORT || 4001;
+
+// const { users } = require("./state");
+
+var express = require('express')
+var app = express()
+var bodyParser = require("body-parser");
+
+//app.use(express.static('public');
+
+app.get('/', (req, res)=> {
+    req.body
+{res.json({name:"JON"}) 
+
+// JSON.stringify()
+}})
+
+//Code here//
+//requied for  body parser//
+app.use(bodyParser.json());
+
+//Part 1//
+app.get("/", (req, res) => {
+	return res.json(users);
+});
 
 const port = process.env.PORT || 4001;
 
 app.listen(port, () => {
- console.log(`Web server is listening on port ${port}!`);
+	console.log(`Web server is listening on port ${port}!`);
 });
