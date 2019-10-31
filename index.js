34a3ed8e15806f19b65d1a4a3734b38b0e62a354
Alex Munoz <anmunoz24@gmail.com>
2019-10-30 18:45:40 -0700

    Started app.get()

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const app = express();
 
 const port = process.env.PORT || 4001;
 
+const comments = require("./data/comments")
+const contacts = require("./data/contacts")
+const products = require("./data/products")
+const vehicles = require("./data/vehicles")
+
+//`app.get()`
+app.get('/comments', (req, res) => {
+  res.json(comments);
+})
+
+app.get('/contacts', (req, res) => {
+  res.json(contacts);
+})
+
+app.get('/products', (req, res) => {
+  res.json(products);
+})
+
+app.get('/vehicles', (req, res) => {
+  res.json(vehicles);
+})
+
+
+
+
+
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
 });
