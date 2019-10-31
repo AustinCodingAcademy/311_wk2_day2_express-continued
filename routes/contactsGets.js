16153daa15cbeab0881908b49ee57760e5866778
Casey Calkins <c.angelcalkins@outlook.com>
2019-10-30 20:46:23 -0500

    Updating code, not finished

---
+++ routes/contactsGets.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+// const router = express.Router()
+// const contacts = require('../data/contacts');
+
+// const app = express;
+
+// app.get('./contacts', (req, res) => {
+
+// })
+
+// app.post('contacts', (req, res) => {
+    
+// })
+
+// app.put('contacts', (req, res) => {
+    
+// })
+
+// app.delete('contacts', (req, res) => {
+    
+// })
+
+// modules.exports = router;
---
+++ routes/controllers/contacts.js
@@ -0,0 +1,5 @@
+// module.exports.list = function(req, res) {
+//     return res.json(comments);
+// }
+
+// module.exports.list = list;
