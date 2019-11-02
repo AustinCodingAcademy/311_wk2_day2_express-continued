649a5d3f1b3d5e50ff9ac962b4cf7d7045251312
David Bille <d_bille@yahoo.com>
2019-11-02 14:18:48 -0500

    routers

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+
 const productRoutes = require('./routes/products')
-
-const port = process.env.PORT || 4001;
-
 app.use(productRoutes)
 
 
-app.get('/vehicles', (req, res) => {
-  
-})
-
-app.post('/products', (req, res) => {
-
-})
 
 
 
-
+const port = process.env.PORT || 4001;
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
 });
