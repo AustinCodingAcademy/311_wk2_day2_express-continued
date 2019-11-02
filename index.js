3c2300c43884c9428b65c62967eab6981f530d55
David Bille <d_bille@yahoo.com>
2019-11-02 16:47:23 -0500

    starting on section 1

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require("express");
 // creates express app
 const app = express();
 
+// // access routes
+// const comments = require('./routes/comments');
+// const contacts = require('./routes/contacts');
+// const products = require('./routes/products');
+// const vehicles = require('./routes/vehicles');
 
+// initializes body-parser
 const bodyParser = require("body-parser");
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(bodyParser.json());
+
+// display static site
+app.use(express.static('public'));
+
+// // callingn functions?
+app.get('/comments', (req, res) => {
+
+});
+
+app.post('/comments', (req, res) => {
+
+});
 
 
 
