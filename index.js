00eb25b28fbdb2565ef51291fc8af551443d5510
Your Name <you@example.com>
2019-11-04 12:09:10 -0600

    prelim Express cont

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 const express = require("express");
+const app = express()
+const path = require ('path');
+app.use('/',express.static('public'));
 const bodyParser = require("body-parser");
-const app = express();
+app.use(bodyParser.json());
+// const contactRouter = require('/routes/contact/');
+// const vehicleRouter = require('/routes/vehicle');
+// const commentsRouter = require('/routes/comments');
+// const productsRouter = require('/routes/products');
 
-const port = process.env.PORT || 4001;
+
+const port = process.env.PORT || 4000;
+const  vehicles = require('./data/vehicles')
+app.get('/vehicles', (req,res) => {
+    console.log(vehicles);
+    res.json(vehicles);
+})
+
+app.get('/contact', (req,res) => {
+    console.log(req.params.id);
+    res.json(contacts);
+})
+app.get('/comments', (req,res) => {
+    console.log(req.params.id);
+    res.json(comments);
+})
+app.get('/products', (req,res) => {
+    console.log(req.params.id);
+    res.json(products);
+})
+
+app.get('/vehicles/:id', (req,res) => {
+    console.log(vehicles);
+    res.json(vehicles);
+})
+
+app.get('/contact/:id', (req,res) => {
+    console.log(req.params.id);
+    res.json(contacts);
+})
+app.get('/comments/:id', (req,res) => {
+    console.log(req.params.id);
+    res.json(comments);
+})
+app.get('/products/:id', (req,res) => {
+    console.log(req.params.id);
+    res.json(products);
+})
 
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
