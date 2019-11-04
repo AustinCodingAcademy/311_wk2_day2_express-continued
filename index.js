502807e9aaeab688ddae73d5e769b737fa9e4e50
devarcher <archershot@hotmail.com>
2019-11-03 18:38:02 -0600

    added vehicles post

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+// Database Folder Variables
 const contacts = require("./data/contacts");
 const vehicles = require("./data/vehicles");
 const comments = require("./data/comments");
@@ -20,13 +22,11 @@ app.use(express.static("public"));
 // Add Body Parser Middleware
 app.use(bodyParser.json());
 
-
 // Routes to all elements in each array
 app.get("/contacts", (req, res) => res.json(contacts));
 app.get("/vehicles", (req, res) => res.json(vehicles));
 app.get("/comments", (req, res) => res.json(comments));
 app.get("/products", (req, res) => res.json(products));
-
 
 // Routes to single element in each array
 app.get("/contacts/:id", (req, res) => {
@@ -62,8 +62,47 @@ app.get("/products/:id", (req, res) => {
   res.json(oneProduct);
 });
 
+// POST Section
+// ID Base Number Variables
+let contactIdBase = contacts.length + 1;
+let vehicleIdBase = vehicles.length + 1;
+let commentsIdBase = comments.length + 1;
+let productsIdBase = products.length + 1;
 
+// Routes to POST new item into Array
+app.post("/contacts", (req, res) => {
+  const newContact = {
+    _id: contactIdBase++,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar,
+    postId: 1
+  };
+  contacts.push(newContact);
+  res.json(contacts);
+});
 
+app.post("/vehicles", (req, res) => {
+  const newVehicle = {
+    _id: vehicleIdBase++,
+    imgUrl: req.body.imgUrl,
+    year: req.body.year,
+    make: req.body.make,
+    model: req.body.model,
+    price: req.body.price,
+    km: req.body.km,
+    miles: req.body.miles,
+    fuel: req.body.fuel,
+    city: req.body.city,
+    isNew: req.body.isNew,
+    postId: 1
+  };
+  vehicles.push(newVehicle);
+  res.json(vehicles);
+});
+
+app.post("/comments", (req, res) => {});
+app.post("/products", (req, res) => {});
 
 // Listener
 app.listen(port, () => {
