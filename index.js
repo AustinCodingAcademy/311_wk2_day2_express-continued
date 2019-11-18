0a4241ad078b60d1c4d97c8683b9dcbdb2297da2
emphillips <matt-phillips@live.com>
2019-11-18 11:30:25 -0600

    Express Cont. Matt Phillips

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-// const data = require(../data)
+
 
 const port = process.env.PORT || 4001;
 app.use(express.static('public'))
