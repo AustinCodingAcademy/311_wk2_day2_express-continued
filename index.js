1ec752ded1c186b834fa466143b7f33daa9cb127
AndrewSimons3 <simdrew07@gmail.com>
2020-01-16 20:49:28 -0600

    started proj

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const contacts = require('./data/contacts.js');
+const vehicles = require('.data/vehicles');
+const comments = require('.data/comments');
+const products = require('.data/products');
 const bodyParser = require("body-parser");
-const app = express();
-
 const port = process.env.PORT || 4001;
+const app = express();
+app.use(express.static('public'))
+bodyParser.json()
+
+
+app.get('/contacts', (req, res) => {
+  res.json(contacts)
+});
+
+app.get('/vehicles', (req, res) => {
+  res.json(vehicles)
+});
+
+app.get('/comments', (req, res) => {
+  res.json(comments)
+});
+
+app.get('/products', (req, res) => {
+  res.json(products)
+});
+
+// app.get('/contacts/:id', (req, res) => {
+//   let searchId = req.params.search
+//   const contact = contacts.find(contact => {
+//     return contact._id === id
+// })
 
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
