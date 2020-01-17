d15419ef1caf6b670be5c120e8ca114919b7c779
pat-white-code <pat.k.white@gmail.com>
2020-01-17 13:58:38 -0600

    routes and controllers complete

--- index.js
+++ index.js
@@ -1,87 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const contacts = require('./data/contacts');
-const vehicles = require('./data/vehicles');
-const products = require('./data/products');
-const comments = require('./data/comments');
 
+//middleware
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/contacts', (req, res) => {
-  res.json(contacts)
-});
-
-app.get('/vehicles', (req, res) => {
-  res.json(vehicles)
-});
-
-app.get('/comments', (req, res) => {
-  res.json(comments)
-});
-
-app.get('/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/contacts/:id', (req, res) => {
-  let id = req.params.id;
-  let contact = contacts.find(contact => contact._id == id);
-  res.json(contact)
-});
-
-app.get('/vehicles/:id', (req, res) => {
-  let id = req.params.id;
-  let vehicle = vehicles.find(vehicle => vehicle._id == id);
-  res.json(vehicle);
-});
-
-app.get('/comments/:id', (req, res) => {
-  let comment = comments.find(comment => comment._id == req.params.id);
-  res.json(comment);
-});
-
-app.get('/products/:id', (req, res) => {
-  let product = products.find(product => product._id == req.params.id);
-  res.json(product);
-});
-
-let vehicleCounter = vehicles.length;
-let commentCounter = comments.length;
-let productCounter = products.length;
-
-app.post('/contacts', (req, res) => {
-  let id = contacts.length + 1;
-  let body = req.body;
-  body.id = id;
-  contacts.push(body);
-  res.json(body);
-})
-
-app.post('/comments', (req, res)=> {
-  commentCounter++
-  let body = req.body;
-  body._id = commentCounter;
-  comments.push(body)
-  res.json(body);
-})
-
-app.post('/vehicles', (req, res)=> {
-  vehicleCounter++
-  let body = req.body;
-  body._id = vehicleCounter;
-  vehicles.push(body);
-  res.json(body);
-})
-
-app.post('/products', (req, res)=> {
-  productCounter++
-  let body = req.body;
-  body._id = productCounter;
-  products.push(body);
-  res.json(body);
-})
+//mounted routers
+app.use('/contacts', require('./routes/contacts'));
+app.use('/vehicles', require('./routes/vehicles'));
+app.use('/comments', require('./routes/comments'));
+app.use('/products', require('./routes/products'));
 
 
 const port = process.env.PORT || 4001;
