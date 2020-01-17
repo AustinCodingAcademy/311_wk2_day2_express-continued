9fdcb5baef111f0bc7225b4595c1231415cd5758
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-01-16 20:47:49 -0600

    done with the hard part. i think.

--- index.js
+++ index.js
@@ -1,6 +1,87 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const contacts = require('./data/contacts');
+const vehicles = require('./data/vehicles');
+const comments = require('./data/comments');
+const products = require('./data/products')
+
+
 const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+// Routes to get all things
+
+app.get('/contacts', (request, response) =>{
+  response.json(contacts);
+})
+
+app.get('/vehicles', (request, response) =>{
+  response.json(vehicles);
+})
+
+app.get('/comments', (request, response) =>{
+  response.json(comments);
+})
+
+app.get('/products', (request, response) =>{
+  response.json(products);
+})
+
+// Routes to get one thing
+
+app.get('/contacts/:id', (request, response) => {
+  let id = request.params.id;
+  return response.json(contacts.find(x => x._id === parseInt(id)));
+})
+
+app.get('/vehiceles/:id', (request, response) => {
+  let id = request.params.id;
+  return response.json(vehicles.find(x => x._id === parseInt(id)));
+})
+
+app.get('/comments/:id', (request, response) => {
+  let id = request.params.id;
+  return response.json(comments.find(x => x._id === parseInt(id)));
+})
+
+app.get('/products/:id', (request, response) => {
+  let id = request.params.id;
+  return response.json(products.find(x => x._id === parseInt(id)));
+})
+
+// Routes to create one thing
+
+app.post('/contacts', (request, response) => {
+  request.body._id = contacts.length;
+  request.body._id++;
+  contacts.push(request.body);
+  return response.json(contacts);
+})
+
+app.post('/vehicles', (request, response) => {
+  request.body._id = vehicles.length;
+  request.body._id++;
+  vehicles.push(request.body);
+  return response.json(vehicles);
+})
+
+app.post('/comments', (request, response) => {
+  request.body._id = comments.length;
+  request.body._id++;
+  comments.push(request.body);
+  return response.json(comments);
+})
+
+app.post('/products', (request, response) => {
+  request.body._id = products.length;
+  request.body._id++;
+  products.push(request.body);
+  return response.json(products);
+})
+
 
 const port = process.env.PORT || 4001;
 
