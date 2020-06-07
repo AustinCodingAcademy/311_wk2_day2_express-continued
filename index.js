3e9ed5b50be5dbd48ed90e772b52959cebda50f0
Sean <contact@seanyager.com>
2020-06-07 16:10:16 -0500

    homework completed

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const commentsRouter = require('./routes/comments');
+const productsRouter = require('./routes/products');
+const vehiclesRouter = require('./routes/vehicles');
+const contactsRouter = require('./routes/contacts');
+
 const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.use(commentsRouter)
+app.use(productsRouter)
+app.use(vehiclesRouter)
+app.use(contactsRouter)
 
 const port = process.env.PORT || 4001;
 
