18bc98795a874b198d4769edf539c4b359548814
TEldridge <skippydrumskins@yahoo.com>
2020-08-15 14:01:07 -0500

    products working

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const contactsRouter = require('./routers/contacts-routers')
-const commentsRouter = require('./routers/comments-router')
+const commentsRouter = require('./routers/comments-routers')
+const productsRouter = require('./routers/products-routers')
 
 const port = process.env.PORT || 4001;
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(contactsRouter)
 app.use(commentsRouter)
+app.use(productsRouter)
 
 
 
