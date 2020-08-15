87b3d96efe7b3b02126622f41fef0966d772f512
TEldridge <skippydrumskins@yahoo.com>
2020-08-15 14:44:41 -0500

    Vehicles working, project complete

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+
+const express = require('express')
 const app = express();
+const bodyParser = require("body-parser");
 const contactsRouter = require('./routers/contacts-routers')
 const commentsRouter = require('./routers/comments-routers')
 const productsRouter = require('./routers/products-routers')
-
+const vehiclesRouter = require('./routers/vehicles-routers')
 const port = process.env.PORT || 4001;
+
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(contactsRouter)
 app.use(commentsRouter)
 app.use(productsRouter)
-
+app.use(vehiclesRouter)
 
 
 app.listen(port, () => {
