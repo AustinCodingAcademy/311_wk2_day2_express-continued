3ce072e70ee0da83b9123cd5868fe40c05b12686
RacMcT <raccmct@gmail.com>
2021-05-12 20:51:40 -0500

    working through part 1

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+app.use(express.static('public'));
 const bodyParser = require("body-parser");
+
+const dataContacts = require("./data/contacts");
+const dataVehicles = require("./data/vehicles");
+const dataComments = require("./data/comments");
+const dataProducts = require ("./data/products");
+
 
 const commentRouter = require("./routers/comments");
 const contactRouter = require("./routers/contacts");
@@ -15,3 +22,4 @@ const port = process.env.PORT || 4001;
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
 });
+
